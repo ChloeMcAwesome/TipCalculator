dec0b0618c6e1fed359a4e7d4b96590875527cc4
Chloe Norris <chloe.norris@willowtreeapps.com>
2020-09-28 14:51:32 -0400

    this is my first commit

--- AzureTestProject/AzureTestProject/ContentView.swift
+++ AzureTestProject/AzureTestProject/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var totalInput: Double? = 18.94
+    @State private var selectedTipPercentage = 0
     
     private var currencyFormatter: NumberFormatter = {
         let f = NumberFormatter()
@@ -17,22 +18,90 @@ struct ContentView: View {
         return f
     }()
     
+    private let tipPercentages = [0.15, 0.2, 0.25]
+    
+    private var percentageFormatter: NumberFormatter = {
+        let f = NumberFormatter()
+        f.numberStyle = .percent
+        return f
+    }()
+    
+    private var tipAmount: Double {
+        let total = totalInput ?? 0
+        let tipPercent = tipPercentages[selectedTipPercentage]
+        return total * tipPercent
+    
+    }
+    
+    private var formattedTipAmount: String {
+        currencyFormatter.string(from: NSNumber(value: tipAmount)) ?? "--"
+    }
+    
+    private var finalTotal: Double {
+        (totalInput ?? 0) + tipAmount
+    }
+   
+    private var formattedFinalTotal: String {
+        currencyFormatter.string(from: NSNumber(value: finalTotal)) ?? "--"
+    }
+    
+    
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(spacing:20) {
                 Spacer()
                 
-                TextF($totalInput, formatter: currencyFormatter)
+                TextField("totalInput", value: $totalInput, formatter: currencyFormatter)
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                selectTip
+                
+                Divider()
+                
+                summaryLine(label: "Tip:", amount: formattedTipAmount, color: .gray)
+                summaryLine(label: "Total:", amount: formattedFinalTotal, color: .green)
+                
                 Spacer()
             }
             .background(Color(white: 0.85, opacity: 1.0))
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .edgesIgnoringSafeArea(.all)
             .navigationBarTitle(Text("Tip Calculator"))
             
         }
         
     }
+    
+    private func summaryLine(label: String, amount: String, color: Color) -> some View {
+        HStack{
+            Spacer()
+            Text(label)
+                .font(.title)
+                .foregroundColor(color)
+            Text(amount)
+                .font(.title)
+                .foregroundColor(color)
+        }.padding(.trailing)
+    }
+    
+    private var selectTip: some View {
+        Picker(selection: $selectedTipPercentage, label: Text("What would you like to tip?")) {
+            ForEach(0..<tipPercentages.count) { index in
+               // let tipPercent = self.tipPercentages[index]
+               // let label = formatPercent(tipPercent)
+               // Text(label).tag(index)
+                Text(self.formatPercent(self.tipPercentages[index])).tag(index)
+            }
+                    }.pickerStyle(SegmentedPickerStyle())
+    }
+    
+    private func formatPercent(_ p: Double) -> String {
+        percentageFormatter.string(from: NSNumber(value: p)) ?? "--"
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
